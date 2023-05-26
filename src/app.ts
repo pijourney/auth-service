b947610bb593138d20d66b3201c207938ecbaba9
Gurk1992 <Gurk1992@users.noreply.github.com>
2023-05-27 01:59:12 +0200

    update cors options

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,7 @@ dotenv.config({ path: ".env" });
 
 // Middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 // livness
