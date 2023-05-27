289f6b23874df7f2c8025e4239843444aed30030
Gurk1992 <Gurk1992@users.noreply.github.com>
2023-05-27 12:00:08 +0200

    add printout in error

--- src/app.ts
+++ src/app.ts
@@ -16,19 +16,20 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 // livness
-app.get("/auth-service/liveness", (req, res) => {
+app.get("/liveness", (req, res) => {
   res.status(200).send("OK");
 });
-app.get("/auth-service/readiness", (req, res) => {
+app.get("/readiness", (req, res) => {
   res.status(200).send("OK");
 });
 
 // Routes
-app.use("/auth-service/api", userRoutes);
-app.use("/auth-service/api/auth", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // 404 Error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req);
   const error = createHttpError(404, "Not Found");
   next(error);
 });
